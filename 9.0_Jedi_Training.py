5ba37b8256049640fa5936a3a7ffa558ae9df0f9
swartzaid <swartzaid@urbandaleschools.com>
2022-04-01 12:28:31 -0500

    Library commit

--- 9.0_Jedi_Training.py
+++ 9.0_Jedi_Training.py
@@ -3,10 +3,10 @@
 
 #1.) Correct the following code: (The user's number should be increased by 1 and printed.)
 
-#def increase():
-    #num = float(input("Enter a number: "))
-    #num+=1
-    #print("Your number has been increased to", num)
+def increase():
+    num = float(input("Enter a number: "))
+    num+=1
+    print("Your number has been increased to", num)
 #increase()
                         
  
@@ -14,11 +14,11 @@
 
 #2. Correct the following code to print 1-10:
 
-#def count_to_ten():
-    #x=0
-    #for i in range(10):
-        #x+=1
-        #print(x)
+def count_to_ten():
+    x=0
+    for i in range(10):
+        x+=1
+        print(x)
  
 #count_to_ten()
 
@@ -26,12 +26,12 @@
 
 #3.) Correct the following code to sum the list:
 
-#def sum_list():
-    #sum=0
-    #list = [45, 2, 10, -5, 100]
-    #for item in list:
-        #sum+=item
-    #print(sum)
+def sum_list():
+    sum=0
+    list = [45, 2, 10, -5, 100]
+    for item in list:
+        sum+=item
+    print(sum)
 #sum_list()
 
 
